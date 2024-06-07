7341a846fa336152ef13ee0819ffed00f9b11153
JosephMA7 <jjmartinez10@utpl.edu.ec>
2024-06-07 11:24:56 -0500

    cambios clase1bi2

--- ejemplo01/genera_tablas.py
+++ ejemplo01/genera_tablas.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
+import datetime 
 
 # se importa información del archivo configuracion
 from configuracion import cadena_base_datos
@@ -35,7 +36,26 @@ class Club(Base):
                           self.nombre, 
                           self.deporte, 
                           self.fundacion)
-
+    
+    def obtener_anios_vida(self):
+        return datetime.datetime.now().year - self.fundacion
+    
+    def obtener_dorsales_jugadores(self):
+        cadena = ""
+        lista = []
+        for l in self.jugadores:
+            cadena = "%s%d\n"% (cadena, l.dorsal)
+        return cadena 
+    
+    def obtener_suma_dorsales(self):
+        #suma = 0
+        #for l in self.jugadores:
+            #suma = suma + l.dorsal
+        #return suma 
+        #Se utilizan listas compresas 
+        suma = sum([s.dorsal for s in self.jugadores])
+        return suma 
+    
 class Jugador(Base):
     __tablename__ = 'jugador'
     id = Column(Integer, primary_key=True)
@@ -52,6 +72,9 @@ class Jugador(Base):
     def __repr__(self):
         return "Jugador: %s - dorsal:%d - posición: %s" % (
                 self.nombre, self.dorsal, self.posicion)
+    
+    def obtener_dorsal_jugador(self):
+        return self.dorsal
 
 Base.metadata.create_all(engine)
 
